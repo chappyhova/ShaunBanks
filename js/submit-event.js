24d9f261120d6bcc5eca0d7021268c504d519527
David Chapman <chappyhova112@googlemail.com>
2018-02-01 19:23:42 +0000

    Workspace changing, AGAIN

---
+++ js/submit-event.js
@@ -0,0 +1,11 @@
+//$(function() {
+ //   $('#submit').click(function() {
+ //       var email = $("#email").val();
+ //       var name = $("#name").val();
+ //       var phone = $("#phoneNumber").val();
+ //       var message = $("#workDescription").val();
+
+ //       return email, name, phone, message;
+
+   // });
+//});
